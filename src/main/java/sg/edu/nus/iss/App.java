e1ea7d7e45fc88d5f2b9991d1e8c426dd8394938
Xin Zhi <115723006+xong002@users.noreply.github.com>
2023-04-17 16:58:26 +0800

    slide 32

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,25 @@
 package sg.edu.nus.iss;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Console cons = System.console();
+
+        List<String> myTodo = new ArrayList<>();
+        String input = "";
+
+        while (!"stop".equals(input)) {
+            input = cons.readLine("? ");
+
+            if (!"stop".equals(input))
+                myTodo.add(input);
+        }
+
+        for (String item : myTodo) {
+            System.out.printf("%s\n", item);
+        }
     }
 }
